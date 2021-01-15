77399bc9c6f92df58cec8d9508ddd849151ae10f
kanbuppa <77398605+kanbuppa@users.noreply.github.com>
2021-01-15 22:43:06 +0700

    Delete hello.java

--- hello.java
+++
@@ -1,8 +0,0 @@
-package test;
-
-public class hello {
-      public static void main(String[] args) {
-      System.out.println("hello from aj ball at CAMT");
-      System.out.println("hello from aj ball at CAMT, CMU.");
-  }
-  }
