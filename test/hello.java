aa7639dfa9b15a3da8a06a42d9d9d3893d08f0e4
kanbuppa <77398605+kanbuppa@users.noreply.github.com>
2021-01-17 23:40:12 +0700

    2 hello

--- test/hello.java
+++ test/hello.java
@@ -1,7 +1,11 @@
+package test;
 
-public class hello {
+
+    public class hello {
       public static void main(String[] args) {
       System.out.println("hello from aj ball at CAMT");
       System.out.println("hello from aj ball at CAMT, CMU.");
   }
   }
+
+
