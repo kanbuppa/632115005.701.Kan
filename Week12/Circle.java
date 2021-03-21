e7c8739e71c8e866c890f4e9d1f222164516b99a
kanbuppa <77398605+kanbuppa@users.noreply.github.com>
2021-03-21 09:24:58 +0700

    Lab12

--- Week12/Circle.java
+++ Week12/Circle.java
@@ -3,9 +3,10 @@ package Week12;
 public class Circle extends Shape{
      private double radius;
 
-     public Circle(double radius){
+     public Circle(double radius) {
          this.radius = radius;
      }
+     
      @Override
      public void getArea(){
          double area;
